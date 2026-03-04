7d8961fc40d3ae6cbc539e85a3ba401828fd6b45
saulmamani <luas0_1@yahoo.es>
2026-03-04 04:36:26 -0400

    escaneno 3

--- verifica-tu-billete/sw.js
+++ verifica-tu-billete/sw.js
@@ -5,7 +5,7 @@
 // forzar la limpieza del caché anterior.
 // ─────────────────────────────────────────────
 
-const CACHE_VERSION = 'v0.62'; // ← incrementa esto en cada deploy
+const CACHE_VERSION = 'v0.63'; // ← incrementa esto en cada deploy
 const CACHE_NAME    = 'verifica-bs-' + CACHE_VERSION;
 
 // Archivos a pre-cachear
